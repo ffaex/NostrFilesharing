6ac5a6f68d9cb1cec74c8ab4e95c46488d07be44
ffaex <you@example.com>
2023-10-07 21:17:29 +0200

    persist relays in local storage

--- components/store.ts
+++ components/store.ts
@@ -1,14 +1,22 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 
 type State = {
-    relays: string[];
-    setRelays: (relays: string[]) => void;
+  relays: string[];
+  setRelays: (relays: string[]) => void;
+};
+
+// Load relays from local storage or use default value
+const loadRelays = () => {
+  const savedRelays = localStorage.getItem('relays');
+  return savedRelays ? JSON.parse(savedRelays) : ['wss://relay.nostrss.re'];
 };
 
 const useStore = create<State>((set) => ({
-    //relays: ['wss://relay.damus.io', 'wss://nos.lol'],
-    relays: ['wss://relay.nostrss.re'],
-    setRelays: (relays) => set({relays}),
+  relays: loadRelays(),
+  setRelays: (relays) => {
+    set({ relays });
+    localStorage.setItem('relays', JSON.stringify(relays));  // Save relays to local storage
+  },
 }));
 
 export default useStore;
